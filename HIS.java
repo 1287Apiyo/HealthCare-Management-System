1dd14de1f043382769ae16138dfcbd1be10b0862
Anne Apiyo <anneapiyo0@gmail.com>
2024-02-22 15:36:22 +0300

    Add files via upload

---
+++ HIS.java
@@ -0,0 +1,9 @@
+public class HIS {
+    public static void main(String[] args) {
+        Patient patient1 =new Patient(2,"Anne","apiyo");
+        Patient patient2 =new Patient(3,"Marion","Apondi");
+
+        patient1.printDetails();
+        patient2.printDetails();
+    }
+}
